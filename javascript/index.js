bb66db291870790b11db47271c0d2bd676c94619
Fabio D <fabio_d89@hotmail.com>
2022-07-03 18:00:53 -0400

    Add listeners for mouse actions

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,31 @@
+let isDrawing = null
 let squares = document.querySelectorAll(".square")
 
 for (let square of squares){
-    console.log (square)
-    square.addEventListener("click", (s) => {
-    s.target.style.backgroundColor = "blue"
-    });
+  square.addEventListener("mousemove", (e) => {
+    if (isDrawing){
+      e.target.style.backgroundColor = "black"
+    }
+      e.target.style.opacity = ".5"
+  });
+
+  square.addEventListener("mouseleave", (e) => {
+    e.target.style.opacity = "1"
+  });
 }
+
+
+// Check when mouse is held down
+window.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+
+// Mouse up
+window.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+
+
+  
+
