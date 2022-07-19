331cb9691338db8030b04cba38480ab3d04b9a57
Fabio D <fabio_d89@hotmail.com>
2022-07-18 20:55:29 -0400

    Add clear button

--- javascript/index.js
+++ javascript/index.js
@@ -2,8 +2,9 @@ let isDrawing = null
 let blackButton = document.querySelector("#blackButton")
 let colorButton = document.querySelector("#colorButton")
 let eraserButton = document.querySelector("#eraserButton")
+let clearButton = document.querySelector("#clearButton")
 
-let color = null
+let color = "black" //default value
 
 blackButton.addEventListener("change", () => {color = "black"})
 colorButton.addEventListener("change", () => {color = "red"})
@@ -11,7 +12,7 @@ eraserButton.addEventListener("change", () => {color = "white"})
 
 
 let drawingArea = document.querySelector("#drawingArea");
-let gridSize = 16; //default value
+let gridSize = 50; //default value
 
 const cellSize = gridSize * gridSize;
 //variable for each "square" within grid
@@ -20,24 +21,21 @@ div.className = "square";
 
 // this will create the grid in the drawing area
 const root = document.documentElement;
+console.log("hey")
 for (let i=0 ; i < cellSize ; i++){
   let div = document.createElement("div");
   div.className = "square";
   drawingArea.appendChild(div);
 }
 
+
 // this is used to adjust CSS grid based on
 // gridSize variable
 root.style.setProperty('--cellSize', gridSize);
 
-
-
 // Check when mouse is held down
 window.addEventListener("mousedown", () => {
   isDrawing = true;
-
-
-
 });
 
 // Check when mouse is held down
@@ -62,8 +60,10 @@ function generateRandomColor() {
 
 
 //For loop through each "square"
+
 let squares = document.querySelectorAll(".square");
 for (let square of squares){
+  clearButton.addEventListener("click", () => {square.style.backgroundColor = "white"})
   //Event lisnter for "Mouse move"
   square.addEventListener("mousemove", (e) => {
     if (isDrawing){
